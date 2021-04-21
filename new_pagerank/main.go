fa283efabfbba884e6a42ef235876e353ae71082
Pranav Kesavarapu <pranavkesavarapu@gmail.com>
2021-04-21 09:41:04 +0530

    datarace with loop variable solved

--- new_pagerank/main.go
+++ new_pagerank/main.go
@@ -37,7 +37,7 @@ func main() {
 	row_ptr[0] = 0
 	cur_row := 0
 	i := 0
-	j := 0
+	// j := 0
 	elrow := 0
 	curel := 0
 
@@ -99,49 +99,50 @@ func main() {
 
 	p_new := make([]float64, n)
 
-	chans := make([]chan float64, n)
-	for i := range chans {
-		chans[i] = make(chan float64, 100)
-	}
+	// chans := make([]chan float64, n)
+	// for i := range chans {
+	// 	chans[i] = make(chan float64, 100)
+	// }
 
 	for looping > 0 {
 		for i = 0; i < n; i++ {
 			p_new[i] = 0.0
-			chans[i] <- 0.0
+			// chans[i] <- 0.0
 		}
 
 		rowel := 0
 		curcol := 0
 
+		type ch chan float64
+		p_channel := make([]ch, n)
+		for i := range p_channel {
+			p_channel[i] = make(chan float64, 1)
+			p_channel[i] <- 0.0
+		}
 		var wg sync.WaitGroup
 		wg.Add(n/granularity + 1)
 
 		for i = 0; i < n; i = i + granularity {
 			rowel = row_ptr[i+1] - row_ptr[i]
-
 			go func(i, rowel, curcol int) {
 				defer wg.Done()
-				for j = 0; j < rowel; j++ {
-					// p_new[col_ind[curcol]] = p_new[col_ind[curcol]] + val[curcol]*p[i]
-					temp := <-chans[col_ind[curcol]]
-					temp += val[curcol] * p[i]
-					chans[col_ind[curcol]] <- temp
+				for j := 0; j < rowel; j++ {
+					temp := <- p_channel[col_ind[curcol]]
+					value := temp + val[curcol] * p[i]
+					p_channel[col_ind[curcol]] <- value
 					curcol++
 				}
 			}(i, rowel, curcol)
 			// p_new[i] = <-chans[i]
-
-			// for _, c := range chans {
-			// 	<-c
-
-			// }
+			curcol += rowel
 		}
-
 		wg.Wait()
-
-		for i := 0; i < n; i++ {
-			p_new[i] = <-chans[i]
+		for i := range p_channel {
+			p_new[i] = <- p_channel[i]
 		}
+		// for i := 0; i < n; i++ {
+		// 	p_new[i] = <-chans[i]
+		// }
 
 		for i = 0; i < n; i++ {
 			p_new[i] = d*p_new[i] + ((1.0 - d) / float64(n))
