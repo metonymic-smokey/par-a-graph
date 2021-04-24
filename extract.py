47c71a09620899ef5d1e8d7e54c6c718c56b70cf
Samyak S Sarnayak <samyak201@gmail.com>
2021-04-24 13:52:29 +0530

    fix extract.py script to avoid blank lines
    also had to format it with black

--- extract.py
+++ extract.py
@@ -1,18 +1,19 @@
-
-
-f = 'input.txt'
-f2 = open('dirLinks.txt','w')
+f = "./input-pageNum.txt"
+f2 = open("pageNum.txt", "w")
 
 with open(f) as fp:
-    line = fp.readline()
-    while line:
-        line = line.strip()
-        l = line.split()
-        l1 = l[0]
-        l2 = " ".join(l[1:])
-        l = [l1,l2]
-        l = ",".join(l) + "\n"
-        f2.write(l)
-        line = fp.readline()
+    for line in fp:
+        if not line.startswith("#"):
+            line = line.strip()
+
+            if len(line) == 0:
+                continue
+
+            l = line.split()
+            l1 = l[0]
+            l2 = " ".join(l[1:])
+            l = [l1, l2]
+            l = ",".join(l) + "\n"
+            f2.write(l)
 
 f2.close()
